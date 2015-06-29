9529d028fe324482fcd0e94c401f2ddc05101639
Luke Iannini <lukexi@me.com>
2015-06-28 23:15:18 -0700

    Add a key command to open the error panel. Add a command to clear the error panel. Normalize spacing. Fix paths sent to ide-backend to be relative to the root folder (which fixes reloading issues and autocomplete not returning any results). Add basic autocompletion support.

--- hide.py
+++ hide.py
@@ -5,9 +5,16 @@ import threading
 import time
 import json
 
-# class ExampleCommand(sublime_plugin.TextCommand):
-#   def run(self, edit):
-#     self.view.insert(edit, 0, "Hello, World!")
+def first_folder(window):
+    return window.folders()[0]
+
+def relative_view_file_name(view):
+    return view.file_name().replace(first_folder(view.window()) + "/", "")
+
+
+class ClearErrorPanelCommand(sublime_plugin.TextCommand):
+    def run(self, edit, errors):
+        self.view.erase(edit, Region(0, self.view.size()))
 
 class UpdateErrorPanelCommand(sublime_plugin.TextCommand):
     def run(self, edit, errors):
@@ -19,7 +26,7 @@ class IdeBackendSaveListener(sublime_plugin.EventListener):
             "request":"updateSession",
             "update": [
                 { "update": "updateSourceFileFromFile"
-                , "filePath": view.file_name()
+                , "filePath": relative_view_file_name(view)
                 }
             ]
             }
@@ -27,18 +34,46 @@ class IdeBackendSaveListener(sublime_plugin.EventListener):
         send_request(view, { "request": "getSourceErrors" })
 
 def send_request(view, request):
-  view.window().run_command("send_ide_backend_request", {"request":request})
+    view.window().run_command("send_ide_backend_request", {"request":request})
 
 class IdeBackendAutocompleteHandler(sublime_plugin.EventListener):
+
+    def __init__(self):
+        super(IdeBackendAutocompleteHandler, self).__init__()
+        self.returnedCompletions = []
+
     def on_query_completions(self, view, prefix, locations):
         request = {
             "request":"getAutocompletion", 
             "autocomplete": {
-                "filePath": view.file_name(), 
+                "filePath": relative_view_file_name(view), 
                 "prefix": prefix
                 } 
             }
         send_request(view, request)
+        # print("Returning: " + str(self.returnedCompletions))
+        print("Returning: " + str([(x, x) for x in self.returnedCompletions]))
+        return [(x, x) for x in self.returnedCompletions]
+
+    def on_window_command(self, window, command_name, args):
+        if args == None:
+            return None
+        completions = args.get("completions")
+        if command_name == "update_completions" and completions:
+            print("INTERCEPTED:")
+            print(completions)
+
+            print()
+            self.returnedCompletions = list(map(lambda x:x.get("name"), completions))
+        return None
+
+class UpdateCompletionsCommand(sublime_plugin.WindowCommand):
+    """
+    This class only exists so that the command can be called and intercepted by
+    IdeBackendAutocompleteHandler to update its completions list.
+    """
+    def run(self, completions):
+        return None
 
 class SendIdeBackendRequestCommand(sublime_plugin.WindowCommand):
     """
@@ -53,21 +88,18 @@ class SendIdeBackendRequestCommand(sublime_plugin.WindowCommand):
         super(SendIdeBackendRequestCommand, self).__init__(window)
         self.boot_ide_backend()
 
-    def first_folder(self):
-        return self.window.folders()[0]
-
     def boot_ide_backend(self):
         """
         Start up a ide-backend-client subprocess for the window, and a thread to consume its stdout
         """
-        print("Launching HIDE in " + self.first_folder())
+        print("Launching HIDE in " + first_folder(self.window))
         print("Backends by window: " + str(self.backendsByWindow))
         self.backendsByWindow[self.window.id()] = "Hi!"
  
  
         self.process = subprocess.Popen(["/Users/lukexi/.cabal/bin/ide-backend-client", "cabal", "."],
             stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-            cwd=self.first_folder()
+            cwd=first_folder(self.window)
             )
         
         self.stdoutThread = threading.Thread(target=self.read_stdout)
@@ -93,19 +125,23 @@ class SendIdeBackendRequestCommand(sublime_plugin.WindowCommand):
 
                 data = json.loads(raw)
                 
-                print(data)
+                # print(data)
                 progress = data.get("progress")
                 response = data.get("response")
                 if progress:
-                  progressMessage = progress.get(parsedMsg)
-                  if progressMessage:
-                    sublime.status_message(progressMessage)
+                    progressMessage = progress.get("parsedMsg")
+                    if progressMessage:
+                        sublime.status_message(progressMessage)
+                elif response == "getAutocompletion":
+                    completions = data.get("completions")
+                    if completions != None:
+                        sublime.set_timeout(lambda: self.update_completions(completions), 0)
                 elif response == "getSourceErrors":
-                  errors = data.get("errors")
-                  if errors:
-                    sublime.set_timeout(lambda: self.highlight_errors(errors), 0)
+                    errors = data.get("errors")
+                    if errors:
+                        sublime.set_timeout(lambda: self.highlight_errors(errors), 0)
                 else:
-                  print(data)
+                    print(data)
                 
             except:
                 print("process ending due to exception:", str(sys.exc_info()))
@@ -114,11 +150,15 @@ class SendIdeBackendRequestCommand(sublime_plugin.WindowCommand):
                 return;
         print("process ended...")
 
+    def update_completions(self, completions):
+        print(completions)
+        self.window.run_command("update_completions", {"completions":completions})
 
     def highlight_errors(self, errors):
         error_panel = self.window.create_output_panel("hide_errors")
         # error_panel.set_read_only(True)
         # error_panel.set_scratch(True)
+        error_panel.run_command("clear_error_panel")
         self.window.run_command("show_panel", {"panel":"output.hide_errors"})
 
         for error in errors:
@@ -149,18 +189,17 @@ class SendIdeBackendRequestCommand(sublime_plugin.WindowCommand):
             self.process = None
 
 def regionFromJSON(span, window):
-  if span == None:
-    return None
-  first_folder = window.folders()[0]
-  file_path    = span.get("filePath")
-  from_line    = span.get("fromLine")
-  from_column  = span.get("fromColumn")
-  to_line      = span.get("toLine")
-  to_column    = span.get("toColumn")
-  full_path    = first_folder + "/" + file_path
-  file_view    = window.find_open_file(full_path)
-  if file_view:
-    from_region = file_view.text_point(from_line - 1, from_column - 1)
-    to_region   = file_view.text_point(to_line   - 1, to_column   - 1)
-    region      = sublime.Region(from_region, to_region)
-    return (region, file_view)
+    if span == None:
+        return None
+    file_path    = span.get("filePath")
+    from_line    = span.get("fromLine")
+    from_column  = span.get("fromColumn")
+    to_line      = span.get("toLine")
+    to_column    = span.get("toColumn")
+    full_path    = first_folder(window) + "/" + file_path
+    file_view    = window.find_open_file(full_path)
+    if file_view:
+        from_point = file_view.text_point(from_line - 1, from_column - 1)
+        to_point   = file_view.text_point(to_line   - 1, to_column   - 1)
+        region      = sublime.Region(from_point, to_point)
+        return (region, file_view)
