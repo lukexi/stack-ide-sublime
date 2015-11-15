559f7c892fef0e55e577c92c94a11c2ddd8d14e1
Rémi Vion <vion.remi@gmail.com>
2015-11-15 12:46:29 +0100

    type popup: colours + links to doc !

--- win.py
+++ win.py
@@ -5,8 +5,9 @@ try:
     import sublime
 except ImportError:
     from test.stubs import sublime
-from utility import first_folder, view_region_from_span, filter_enclosing, shorten_module_prefixes
+from utility import first_folder, view_region_from_span, filter_enclosing, format_type
 from response import parse_source_errors, parse_exp_types
+import webbrowser
 
 class Win:
     """
@@ -49,7 +50,7 @@ class Win:
                 view.set_status("type_at_cursor", _type)
                 view.add_regions("type_at_cursor", [view_region_from_span(view, span)], "storage.type", "", sublime.DRAW_OUTLINED)
                 if Win.show_popup:
-                    view.show_popup(shorten_module_prefixes(_type))
+                    view.show_popup(format_type(_type), on_navigate= (lambda href: webbrowser.open(href)))
                 return
 
         # Clear type-at-cursor display     
