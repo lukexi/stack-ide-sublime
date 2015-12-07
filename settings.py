379fa489fe5e7e79996c68d122f6d6cfa474da3c
Luke Iannini <lukexi@me.com>
2015-12-06 20:01:37 -0800

    Fix tests

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 class Settings:
 
-    def __init__(self, verbosity, add_to_PATH, show_popup, hoogle_url):
+    def __init__(self, verbosity, add_to_PATH, show_popup, hoogle_url=None):
         self.verbosity = verbosity
         self.add_to_PATH = add_to_PATH
         self.show_popup = show_popup
